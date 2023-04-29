c3af39259506cfc1246a81f9b1c9ef4bcd6a1f81
Aigerim <t_sa06@mail.ru>
2023-04-29 11:17:07 +0600

    Add project files.

---
+++ ToDoDDD.DAL/Entitities/Issue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoDDD.DAL.Entitities
+{
+    public class Issue:BaseEntity
+    {
+        public string Name { get;set; }
+        public Priority? Priority { get;set; }   
+        public Status? Status { get;set; }
+    }
+}
---
+++ ToDoDDD.DAL/Entitities/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoDDD.DAL.Entitities
+{
+    public class Status:BaseEntity
+    {
+        public string Name { get; set; }
+        public virtual List<Issue> Issues { get; set; }
+    }
+}
